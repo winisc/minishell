71483630486e1d539f54f821da76c49c26387774
Winicius Silveira <winiciussilveira@gmail.com>
2026-04-22 13:28:21 -0300

    fix: fix norminette error

--- src/process_input.c
+++ src/process_input.c
@@ -6,31 +6,32 @@
 /*   By: wini <wini@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 13:25:03 by wini              #+#    #+#             */
-/*   Updated: 2026/04/22 13:25:03 by wini             ###   ########.fr       */
+/*   Updated: 2026/04/22 13:27:47 by wini             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /* Processa entrada do usuário através do pipeline completo */
+
+/* TODO: Lexer - Tokenizar entrada */
+/* t_token *tokens = lexer(input); */
+
+/* TODO: Parser - Construir AST */
+/* t_ast *ast = parser(tokens); */
+
+/* TODO: Executor - Executar comandos */
+/* execute(ast); */
+
+/* TODO: Liberar recursos */
+/* free_tokens(tokens); */
+/* free_ast(ast); */
+
+/* TODO: Detectar comando "exit" */
+/* if (check_exit_command(input)) */
+/*     return (EXIT_REQUESTED); */
+
 int	process_input(char *input)
 {
-	/* TODO: Lexer - Tokenizar entrada */
-	/* t_token *tokens = lexer(input); */
-
-	/* TODO: Parser - Construir AST */
-	/* t_ast *ast = parser(tokens); */
-
-	/* TODO: Executor - Executar comandos */
-	/* execute(ast); */
-
-	/* TODO: Liberar recursos */
-	/* free_tokens(tokens); */
-	/* free_ast(ast); */
-
-	/* TODO: Detectar comando "exit" */
-	/* if (check_exit_command(input)) */
-	/*     return (EXIT_REQUESTED); */
-
 	return (CONTINUE_SHELL);
 }
